dc4755b416a3408c21ef5e398a8044b3943a35e1
ezioauditore-tech <sakthivelpillai25@gmail.com>
2022-06-01 13:25:30 +0530

    commit

--- app/src/main/java/com/example/valetparking/MainActivity.java
+++ app/src/main/java/com/example/valetparking/MainActivity.java
@@ -2,18 +2,34 @@ package com.example.valetparking;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.huawei.hmf.tasks.Task;
+import com.huawei.hms.common.ApiException;
+import com.huawei.hms.support.account.AccountAuthManager;
+import com.huawei.hms.support.account.request.AccountAuthParams;
+import com.huawei.hms.support.account.request.AccountAuthParamsHelper;
+import com.huawei.hms.support.account.result.AuthAccount;
+import com.huawei.hms.support.account.service.AccountAuthService;
+import com.huawei.hms.support.hwid.ui.HuaweiIdAuthButton;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView back;
-    Button login;
+    Button login;Button signin;
+    HuaweiIdAuthButton huaweiIdAuthButton;
+    private AccountAuthService mAuthService;
+    private AccountAuthParams mAuthParam;
+    private static final int REQUEST_CODE_SIGN_IN=1000;
+    private static final String TAG="Account";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +39,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         back = findViewById(R.id.back);
         back.setOnClickListener(view -> finish());
-
-        login = findViewById(R.id.loginButton);
-        login.setOnClickListener(new View.OnClickListener() {
+        huaweiIdAuthButton=findViewById(R.id.hwid_signin);
+        huaweiIdAuthButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this,homepage.class);
-                startActivity(i);
+                AccountAuthParams authParams = new AccountAuthParamsHelper(AccountAuthParams.DEFAULT_AUTH_REQUEST_PARAM).setAuthorizationCode().createParams();
+                AccountAuthService service = AccountAuthManager.getService(MainActivity.this, authParams);
+                startActivityForResult(service.getSignInIntent(), 8888);
+
             }
         });
 
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // Process the authorization result to obtain the authorization code from AuthAccount.
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 8888) {
+            Task<AuthAccount> authAccountTask = AccountAuthManager.parseAuthResultFromIntent(data);
+            if (authAccountTask.isSuccessful()) {
+                // The sign-in is successful, and the user's ID information and authorization code are obtained.
+                AuthAccount authAccount = authAccountTask.getResult();
+                startActivity(new Intent(MainActivity.this,Drawer.class));
+                finish();
+                Log.i(TAG, "serverAuthCode:" + authAccount.getAuthorizationCode());
+            } else {
+                // The sign-in failed.
+                Log.e(TAG, "sign in failed:" + ((ApiException) authAccountTask.getException()).getStatusCode());
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/valetparking/Splash_Screen.java
@@ -0,0 +1,23 @@
+package com.example.valetparking;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash_Screen.this,MainActivity.class));
+                finish();
+            }
+        },2000);
+    }
+}
